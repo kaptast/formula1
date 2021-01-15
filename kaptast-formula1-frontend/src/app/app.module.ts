31d37638966796199ee3ea0ebc13a16cefef1c1b
kaptast <kaptas.tamas@gmail.com>
2021-01-15 23:23:22 +0100

    add components to app

--- kaptast-formula1-frontend/src/app/app.module.ts
+++ kaptast-formula1-frontend/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './teams/teams.component';
+import { TeamComponent } from './team/team.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent,
+    TeamComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
