219f7c68e89b3e15b1da3a1635f93fb6211d915b
kaptast <kaptas.tamas@gmail.com>
2021-01-15 17:22:50 +0100

    authservice

--- kaptast-formula1-backend/kaptast-formula1-api.Services/Interfaces/IAuthService.cs
+++ kaptast-formula1-backend/kaptast-formula1-api.Services/Interfaces/IAuthService.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using kaptast_formula1_api.ViewModels.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace kaptast_formula1_api.Services.Interfaces
 {
     public interface IAuthService
     {
+        Task<SignInResult> Login(UserViewModel user);
+        void LogOff();
     }
 }
