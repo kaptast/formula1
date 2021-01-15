dca25d3c04516700f781e79e5c2f119e8e3dfe0c
kaptast <kaptas.tamas@gmail.com>
2021-01-15 17:20:51 +0100

    remove unused repository

--- kaptast-formula1-backend/kaptast-formula1-api.Repository/Repositories/Interfaces/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using kaptast_formula1_api.Repository.Entities;
-
-namespace kaptast_formula1_api.Repository.Repositories.Interfaces
-{
-    public interface IUserRepository : IRepository<User>
-    {
-    }
-}
