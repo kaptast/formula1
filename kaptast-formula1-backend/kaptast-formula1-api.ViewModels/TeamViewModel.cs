8c2af08e9b5a156ee64ffb5df06c54e7f1427b48
kaptast <kaptas.tamas@gmail.com>
2021-01-15 15:07:55 +0100

    add ViewModel project
    UserViewModel and TeamViewModel classes

---
+++ kaptast-formula1-backend/kaptast-formula1-api.ViewModels/TeamViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kaptast_formula1_api.ViewModels
+{
+    public class TeamViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime YearOfFoundation { get; set; }
+        public int ChampionshipsWon { get; set; }
+        public bool EntryPaid { get; set; }
+    }
+}
---
+++ kaptast-formula1-backend/kaptast-formula1-api.ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace kaptast_formula1_api.ViewModels
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
