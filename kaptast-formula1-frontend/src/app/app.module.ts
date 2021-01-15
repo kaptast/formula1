34760110b87248d62def111c37ab1fa3b04ffc2b
kaptast <kaptas.tamas@gmail.com>
2021-01-16 00:04:33 +0100

    import formsmodule for updating team data

--- kaptast-formula1-frontend/src/app/app.module.ts
+++ kaptast-formula1-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TeamsComponent } from './teams/teams.component';
@@ -16,6 +17,7 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
