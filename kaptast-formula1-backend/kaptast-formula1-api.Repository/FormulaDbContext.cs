4171406283d1a74faede104c4f1355cd3d06d989
kaptast <kaptas.tamas@gmail.com>
2021-01-15 13:26:51 +0100

    add formula1 application database context

---
+++ kaptast-formula1-backend/kaptast-formula1-api.Repository/FormulaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using kaptast_formula1_api.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace kaptast_formula1_api.Repository
+{
+    public class FormulaDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(":memory:");
+        }
+    }
+}
