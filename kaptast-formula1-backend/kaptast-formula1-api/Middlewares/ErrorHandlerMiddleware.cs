156dce48d8eef98aac5f3fd3c3816221ba0fb6ef
kaptast <kaptas.tamas@gmail.com>
2021-01-18 13:01:58 +0100

    error handling middleware

---
+++ kaptast-formula1-backend/kaptast-formula1-api/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace KaptastFormula1Api.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ErrorHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<ErrorHandlerMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                _logger.LogError(error.Message, error);
+
+                switch (error)
+                {
+                    case ArgumentNullException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    case DbUpdateException e:
+                        response.StatusCode = (int)HttpStatusCode.Conflict;
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
